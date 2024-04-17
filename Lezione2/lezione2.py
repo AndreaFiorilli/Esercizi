14e0b5a83e9e3f5498dfb9cdd5f2555aa7d669b8
AndreaFiorilli <andreafiorilli04@gmail.com>
2024-04-17 13:49:08 +0200

    File lezione2.py aggiunto

---
+++ Lezione2/lezione2.py
@@ -0,0 +1,20 @@
+# Andrea Fiorilli
+# 17/04/2024
+
+print("Hello World!")
+
+# 2-3. Personal Message: Use a variable to represent a person’s name, 
+# and print a message to that person.
+# Your message should be simple, such as, 
+# “Hello Eric, would you like to learn some Python today?”
+
+name: str =("Luca")
+message: str=f"Hello {name}, would you like to learn some python today?"
+print(message)
+
+# 2-4. Name Cases: Use a variable to represent a person’s name, 
+# and then print that person’s name in lowercase, uppercase, 
+#and title case.
+
+name: str =("Luca")
+print(f"{name.lower()}\n{name.upper()}\n{name.title()}\n")
