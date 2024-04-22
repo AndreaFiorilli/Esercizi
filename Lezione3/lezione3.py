d2c56db3e14c5dcc244f4f269a92bbbbbc1afea3
AndreaFiorilli <andreafiorilli04@gmail.com>
2024-04-22 13:16:32 +0200

    aggiornato

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -68,4 +68,67 @@ for a in num:
 # and use a for loop to print out the value of each cube.
 
 
+num: list = range(1,11)
+for a in num:
+    b=a**3
+    print(b)
 
+# 4-9. Cube Comprehension: Use a list comprehension to generate a list of the first 10 cubes.
+
+lista: list = [lista ** 3 for lista in range(1,11)]
+print(lista)
+
+# 4-10. Slices: Using one of the programs you wrote in this chapter, add several lines to the end of the program that do the following:
+# • Print the message The first three items in the list are:. Then use a slice to print the first three items from that program’s list.
+# • Print the message Three items from the middle of the list are:. Then use a slice to print three items from the middle of the list.
+# • Print the message The last three items in the list are:. Then use a slice to print the last three items in the list.
+
+num: list = [lista for lista in range(1,11)]
+for a in num:
+    print(a)
+print (f"The first three items in the list are: {num[:3]}")
+print (f"Three items from the middle of the list are: {num[3:6]}")
+print (f"The first three items in the list are: {num[7:]}")
+
+# 4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1. 
+# Make a copy of the list of pizzas, and call it friend_pizzas. Then, do the following:
+# • Add a new pizza to the original list.
+# • Add a different pizza to the list friend_pizzas.
+# • Prove that you have two separate lists. Print the message My favorite pizzas are:, and then use a for loop to print the first list. Print the message My friend’s favorite pizzas are:, and then use a for loop to print the second list. Make sure each new pizza is stored in the appropriate list.
+
+a: list = ["Margherita","Diavola","Boscaiola"]
+for sentence in a:
+    print (f"I like pizza {sentence}")
+print ("I really like pizza")
+z: str = ""
+y: str = ""
+
+friend_pizzas: list = a.copy() 
+a.append("Bianca")
+friend_pizzas.append("Rossa")
+for sentence in a:
+    z=z+", "+sentence
+print (f"My favorite pizzas are:{z}")
+for sentence1 in friend_pizzas:
+    y=y+", "+sentence1
+print (f"My friend’s favorite pizzas are:{y}")
+
+# 4-12. More Loops: All versions of foods.py in this section have avoided using for loops when printing, to save space. Choose a version of foods.py, and write two for loops to print each list of foods.
+
+# 5-1. Conditional Tests: Write a series of conditional tests. Print a statement
+# describing each test and your prediction for the results of each test. Your code
+# should look something like this:
+# car = 'subaru'
+# print("Is car == 'subaru'? I predict True.")
+# print(car == 'subaru')
+# print("\nIs car == 'audi'? I predict False.")
+# print(car == 'audi')
+# • Look closely at your results, and make sure you understand why each line
+# evaluates to True or False.
+# • Create at least 10 tests. Have at least 5 tests evaluate to True and another
+# 5 tests evaluate to False.
+
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi') 
